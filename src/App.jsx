c7d9a27ed37a4fb4326a86a750435c8de91b0130
Thamires Lopes <thamiresl686@gmail.com>
2021-02-12 17:41:47 -0300

    Passing children to MyCard and layout adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import {
+  Card, Button, Container, Row,
+} from 'react-bootstrap';
 import MyCard from './components/Card';
 import MyHeader from './components/Card/Header';
+import code from './images/code.jpg';
 
 function App() {
   return (
     <div>
-      <MyHeader title="Pitang" routes={['Primeira rota', 'Segunda rota', 'Terceira rota']} />
-      <MyCard title="Home" />
+      <MyHeader title="Pitang" routes={['First route', 'Second route', 'Third route']} />
+      <div className="m-3">
+        <MyCard title="Home">
+          <Button variant="dark">Add Info</Button>
+          <Card.Subtitle className="mb-2 text-muted - text-center">
+            There are no itens yet.
+          </Card.Subtitle>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Card.Img className="justify-content-md-center" style={{ width: '50rem' }} variant="bottom" src={code} />
+            </Row>
+          </Container>
+        </MyCard>
+      </div>
     </div>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import {
-  Card, Button, Container, Row,
+  Card,
 } from 'react-bootstrap';
-import code from '../../images/code.jpg';
 
-const MyCard = ({ title }) => (
+const MyCard = ({ title, children }) => (
   <Card className="mb-4">
     <Card.Header>
       <Card.Title>{title}</Card.Title>
     </Card.Header>
     <Card.Body>
-      <Button variant="dark">Add Info</Button>
-      <Card.Subtitle className="mb-2 text-muted - text-center">
-        There are no itens yet.
-      </Card.Subtitle>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Card.Img className="justify-content-md-center" style={{ width: '50rem' }} variant="bottom" src={code} />
-        </Row>
-      </Container>
-
+      {children}
     </Card.Body>
   </Card>
 );

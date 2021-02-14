d7779cb01527b5706a5c5bbdd2897e1bc925009f
Thamires Lopes <thamiresl686@gmail.com>
2021-02-14 11:44:39 -0300

    Correcting end of line

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ const MyHeader = ({ title, routes = [] }) => (
   <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="#home">{title}</Navbar.Brand>
     <Nav className="mr-auto">
-      { routes.map((route) => <Nav.Link href="#">{route}</Nav.Link>)}
+      {routes.map((route) => <Nav.Link href="#">{route}</Nav.Link>)}
     </Nav>
   </Navbar>
 );
